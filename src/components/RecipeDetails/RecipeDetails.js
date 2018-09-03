a3b701333f7dff05edf70ded700b7f0b09b9b0e5
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-09-03 21:25:42 +0200

    add recipedetails component

---
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RecipeDetails = (props) => {
+
+ 
+    return (
+        <div>
+       
+        one recipe
+        </div>
+    )
+
+}
+
+export default RecipeDetails;
